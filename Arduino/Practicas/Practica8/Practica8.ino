a52643f48c1e76b2309b492dd7a8754d6a5e548d
Ludwig Ivan <ivansierras11@gmail.com>
2025-02-26 20:48:20 -0600

    Act 8 (completa)

--- Arduino/Practicas/Practica8/Practica8.ino
+++ Arduino/Practicas/Practica8/Practica8.ino
@@ -10,18 +10,19 @@ void setup() {
 }
 
 void cleanPins(){
-  digitalWrite(2,LOW);
-  digitalWrite(3,LOW);
-  digitalWrite(4,LOW);
-  digitalWrite(5,LOW);
-  digitalWrite(6,LOW);
-  digitalWrite(7,LOW);
-  digitalWrite(8,LOW);
+  bool ban = true;
+  digitalWrite(2,ban);
+  digitalWrite(3,ban);
+  digitalWrite(4,ban);
+  digitalWrite(5,ban);
+  digitalWrite(6,ban);
+  digitalWrite(7,ban);
+  digitalWrite(8,ban);
 }
 
 void onPins(int pins[], int tam){
   for (int cont=0; cont<tam; cont++) {
-      digitalWrite(pins[cont],HIGH);
+      digitalWrite(pins[cont],LOW);
       Serial.println(pins[cont]);
   }
 }
@@ -30,12 +31,12 @@ void loop() {
   // put your main code here, to run repeatedly:
   cleanPins();
   int cero[]={8,7,6,5,4,3};
-  int uno[]={4,3};
+  int uno[]={7,6};
   int dos[]={8,7,2,4,5};
   int tres[]={8,7,2,6,5};
   int cuatro[]={3,2,7,6};
   int cinco[]={8,3,2,6,5};
-  int seis[]={7,6,5,4,2};
+  int seis[]={8,3,4,2,6,5};
   int siete[]={8,7,6};
   int ocho[]={8,7,6,5,4,3,2};
   int nueve[]={8,3,7,2,6};

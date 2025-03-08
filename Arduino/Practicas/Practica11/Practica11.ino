2ed5794485f7349ed17c4e7d79b35d1921af312d
Ludwig Ivan <ivansierras11@gmail.com>
2025-03-08 17:50:39 -0600

    conflicto

--- Arduino/Practicas/Practica11/Practica11.ino
+++ Arduino/Practicas/Practica11/Practica11.ino
@@ -1,7 +1,8 @@
-#include <LiquidCrystal.h>
+#include <Wire.h> 
+#include <LiquidCrystal_I2C.h>
 
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+LiquidCrystal_I2C lcd(0x27,20,4);
 const int limit_time=1000;
 unsigned long last_time = 0; 
 String text="";
@@ -9,11 +10,25 @@ String text="";
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  lcd.begin(16,2);
+  lcd.init(); // Inicializa el LCD
+  lcd.backlight(); // Enciende la luz de fondo
+
+    // Escribir en las 4 líneas del LCD
+    lcd.setCursor(0, 0);
+    lcd.write("Linea 1: Hola!");
+    
+    lcd.setCursor(0, 1);
+    lcd.print("Linea 2: Arduino");
+    
+    lcd.setCursor(0, 2);
+    lcd.print("Linea 3: 20x4 LCD");
+    
+    lcd.setCursor(0, 3);
+    lcd.print("Linea 4: I2C OK");
 }
 
 void loop() {
-  // Hago la lectura de la informacion enviada por el serial monitor
+  /* Hago la lectura de la informacion enviada por el serial monitor
   while(Serial.available()>0){
     char letter = Serial.read();
     text+=letter;
@@ -22,7 +37,7 @@ void loop() {
 
   /* Si tengo algun texto y el tiempo en que lo escribi supera los 500 ms se considera
   que no hay mas info q escribir, y la muestra en el lcd
-  */
+  /
   if (text.length() > 0 && millis() - last_time > limit_time) {
     lcd.clear();
     lcd.print(text);
@@ -44,5 +59,5 @@ void loop() {
     }
     
     text = "";
-  }
+  }*/
 }
--- Arduino/Practicas/Practica9/Practica9.ino
+++ Arduino/Practicas/Practica9/Practica9.ino
@@ -6,20 +6,34 @@ int cant=8;
 int pins[8]={2,3,4,5,6,7,8,9};
 
 void setup() {
-  // put your setup code here, to run once:
+    // put your setup code here, to run once:
   for(int i=0;i<cant;i++){
     pinMode(pins[i],OUTPUT);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i=0;i<cant;i++){
-    digitalWrite(pins[i],HIGH);
-    delay(100);
+  // Encender LEDs en una dirección
+  for (int i = 0; i < cant; i++) {
+    digitalWrite(pins[i], HIGH);
+    if (i > 0) {
+      digitalWrite(pins[i - 1], LOW);
+    }
+    delay(20);
   }
-  for(int i=0;i<cant;i++){
-    digitalWrite(pins[i],LOW);
-    delay(100);
+
+  delay(10);
+  
+  // Apagar el último LED encendido
+  digitalWrite(pins[cant - 1], LOW);
+
+  // Encender LEDs en la dirección opuesta
+  for (int i = cant - 1; i >= 0; i--) {
+    digitalWrite(pins[i], HIGH);
+    if (i < cant - 1) {
+      digitalWrite(pins[i + 1], LOW);
+    }
+    delay(20);
   }
 }
+
